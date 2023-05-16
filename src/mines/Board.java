6a9f4553940eb7c653c6785488480c043b908a39
rachalabidi <rashalabidi@gmail.com>
2023-05-16 02:00:07 +0200

    adding Junit and some tests

--- src/mines/Board.java
+++ src/mines/Board.java
@@ -10,25 +10,28 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-
+/**
+ * The Board class represents the game board for a Minesweeper game.
+ */
 public class Board extends JPanel {
+    // Constants for cell states
 	private static final long serialVersionUID = 6195235521361212179L;
 	
 	private static final int NUM_IMAGES = 13;
     private static final int CELL_SIZE = 15;
 
-    private static final int COVER_FOR_CELL = 10;
+    public static final int COVER_FOR_CELL = 10;
     private static final int MARK_FOR_CELL = 10;
-    private static final int EMPTY_CELL = 0;
+    public static final int EMPTY_CELL = 0;
     private static final int MINE_CELL = 9;
-    private static final int COVERED_MINE_CELL = MINE_CELL + COVER_FOR_CELL;
+    public static final int COVERED_MINE_CELL = MINE_CELL + COVER_FOR_CELL;
     private static final int MARKED_MINE_CELL = COVERED_MINE_CELL + MARK_FOR_CELL;
-
+    // Constants for drawing images
     private static final int DRAW_MINE = 9;
     private static final int DRAW_COVER = 10;
     private static final int DRAW_MARK = 11;
     private static final int DRAW_WRONG_MARK = 12;
-
+    // Instance variables
     private int[] field;
     private boolean inGame;
     private int minesLeft;
@@ -38,9 +41,37 @@ public class Board extends JPanel {
     private int cols = 16;
     private int allCells;
     private JLabel statusbar;
+
+    public boolean isInGame() {
+        return inGame;
+    }
+
+    public int getMines() {
+        return mines;
+    }
+
+    public int getMinesLeft() {
+        return minesLeft;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getCols() {
+        return cols;
+    }
+
+    public int getAllCells() {
+        return allCells;
+    }
+
+    public int[] getField() {
+        return field;
+    }
     SecureRandom random = new SecureRandom();
 
-
+    // Constructors
     public Board(JLabel statusbar) {
 
         this.statusbar = statusbar;
@@ -60,12 +91,18 @@ public class Board extends JPanel {
     }
 
 
+    /**
+     * Initializes a new game.
+     */
     public void newGame() {
         initializeGame();
         deployMines();
     }
 
-    private void initializeGame() {
+    /**
+     * Initializes the game state.
+     */
+    public void initializeGame() {
         inGame = true;
         minesLeft = mines;
         allCells = rows * cols;
@@ -76,7 +113,10 @@ public class Board extends JPanel {
         statusbar.setText(Integer.toString(minesLeft));
     }
 
-    private void deployMines() {
+    /**
+     * Deploys the mines on the game board.
+     */
+    public void deployMines() {
 
         int minesDeployed = 0;
         while (minesDeployed < mines) {
@@ -88,15 +128,22 @@ public class Board extends JPanel {
             }
         }
     }
-
-    private void incrementAdjacentCells(int position) {
+    /**
+     * Increments the count of adjacent cells for a given mine position.
+     *
+     * @param position The position of the mine cell.
+     */
+    public void incrementAdjacentCells(int position) {
         int currentCol = position % cols;
 
-        incrementCellIfValid(position - 1 - cols);
-        incrementCellIfValid(position - 1);
-        incrementCellIfValid(position + cols - 1);
+        incrementCellIfValid(position - cols - 1);
         incrementCellIfValid(position - cols);
+        incrementCellIfValid(position - cols + 1);
+        incrementCellIfValid(position - 1);
+        incrementCellIfValid(position + 1);
+        incrementCellIfValid(position + cols - 1);
         incrementCellIfValid(position + cols);
+        incrementCellIfValid(position + cols + 1);
 
         if (currentCol < (cols - 1)) {
             incrementCellIfValid(position - cols + 1);
@@ -110,13 +157,22 @@ public class Board extends JPanel {
             field[cell]++;
         }
     }
-
-    private boolean isValidCell(int cell) {
+    /**
+     * Checks if a cell is a valid cell in the game board.
+     *
+     * @param cell The cell index to check.
+     * @return True if the cell is valid, false otherwise.
+     */
+    public boolean isValidCell(int cell) {
         return cell >= 0 && cell < allCells;
     }
 
 
-
+    /**
+     * Finds empty cells and merges them recursively.
+     *
+     * @param j The starting cell index.
+     */
     public void findEmptyCells(int j) {
         int currentCol = j % cols;
 
@@ -133,6 +189,11 @@ public class Board extends JPanel {
         }
     }
 
+    /**
+     * Merges the cell with its adjacent cells if the cell is valid.
+     *
+     * @param cell The cell index.
+     */
     private void mergeIfValid(int cell) {
         if (isValidCell(cell) && field[cell] > MINE_CELL) {
             field[cell] -= COVER_FOR_CELL;
@@ -161,7 +222,12 @@ public class Board extends JPanel {
         }
         updateGameStatus(uncover);
     }
-
+    /**
+     * Determines the image index for the given cell.
+     *
+     * @param cell The cell value.
+     * @return The image index.
+     */
     private int getCellDrawing(int cell) {
         switch (cell) {
             case COVERED_MINE_CELL:
@@ -179,14 +245,24 @@ public class Board extends JPanel {
                 return cell;
         }
     }
-
+    /**
+     * Updates the count of uncovered cells.
+     *
+     * @param uncover The current count of uncovered cells.
+     * @param cell    The cell value.
+     * @return The updated count of uncovered cells.
+     */
     private int updateUncoverCount(int uncover, int cell) {
         if (cell > MINE_CELL) {
             return uncover + 1;
         }
         return uncover;
     }
-
+    /**
+     * Updates the game status based on the count of uncovered cells.
+     *
+     * @param uncover The count of uncovered cells.
+     */
     private void updateGameStatus(int uncover) {
         if (uncover == 0 && inGame) {
             inGame = false;
@@ -198,6 +274,9 @@ public class Board extends JPanel {
 
 
 
+    /**
+     * The MinesAdapter class handles mouse events on the game board.
+     */
     class MinesAdapter extends MouseAdapter {
 
         @Override
@@ -227,11 +306,23 @@ public class Board extends JPanel {
                 repaint();
             }
         }
-
+        /**
+         * Checks if the cell click is within the valid bounds.
+         *
+         * @param x The x-coordinate of the click.
+         * @param y The y-coordinate of the click.
+         * @return True if the cell click is valid, false otherwise.
+         */
         private boolean isValidCellClick(int x, int y) {
             return (x < cols * CELL_SIZE) && (y < rows * CELL_SIZE);
         }
 
+        /**
+         * Handles the right click on a cell.
+         *
+         * @param cRow The row index of the clicked cell.
+         * @param cCol The column index of the clicked cell.
+         */
         private void handleRightClick(int cRow, int cCol) {
             if (field[(cRow * cols) + cCol] > MINE_CELL) {
                 if (field[(cRow * cols) + cCol] <= COVERED_MINE_CELL) {
@@ -250,7 +341,13 @@ public class Board extends JPanel {
             }
         }
 
-        private void handleLeftClick(int cRow, int cCol) {
+        /**
+         * Handles the left click on a cell.
+         *
+         * @param cRow The row index of the clicked cell.
+         * @param cCol The column index of the clicked cell.
+         */
+        public void handleLeftClick(int cRow, int cCol) {
             if (field[(cRow * cols) + cCol] > COVERED_MINE_CELL) {
                 return;
             }
