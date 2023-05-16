1a82d591ea51d00377e09577303aef4a0760de68
rachalabidi <rashalabidi@gmail.com>
2023-05-16 02:07:39 +0200

    adding Junit and some tests

--- src/mines/Mines.java
+++ src/mines/Mines.java
@@ -5,7 +5,10 @@ import java.awt.BorderLayout;
 import javax.swing.*;
 
 // Source: http://zetcode.com/tutorials/javagamestutorial/minesweeper/
-
+/**
+ * The Mines class represents the main application window for the Minesweeper game.
+ * It extends the JFrame class and creates the game window.
+ */
 public class Mines extends JFrame {
 	private static final long serialVersionUID = 4772165125287256837L;
 	
@@ -13,7 +16,10 @@ public class Mines extends JFrame {
     private static final int HEIGHT = 290;
 
     private JLabel statusbar;
-    
+    /**
+     * Constructs a new Mines object.
+     * Sets up the game window and initializes the game board.
+     */
     public Mines() {
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -23,14 +29,22 @@ public class Mines extends JFrame {
 
         statusbar = new JLabel("");
         add(statusbar, BorderLayout.SOUTH);
+        // Add an instance of the Board class (the game board) to the frame
 
         add(new Board(statusbar));
 
         setResizable(false);
         setVisible(true);
     }
-    
+    /**
+     * The main method of the Mines class.
+     * Creates an instance of the Mines class to start the game.
+     *
+     * @param args the command-line arguments
+     */
     public static void main(String[] args) {
+        // Create an instance of the Mines class to start the game
+
         new Mines();
     }
 }
